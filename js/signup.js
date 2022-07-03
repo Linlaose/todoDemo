e1f4e16a2a549a77a4878ea1e7276cead94ac962
Linlaose <a0986575797@gmail.com>
2022-07-04 03:30:37 +0800

    註冊功能完成

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,51 @@
 const accountSignUp = document.querySelector('.account-signup');
 const passwordSignUp = document.querySelector('.password-signup');
-const nickname = document.querySelector('.nickname-signup');
+const nicknameSignUp = document.querySelector('.nickname-signup');
 const signUp = document.querySelector('.signup');
+const urlAPI = "https://todoo.5xcamp.us";
+let obj = {};
+
+function callSignUp() {
+  const email = accountSignUp.value;
+  const nickname = nicknameSignUp.value;
+  const password = passwordSignUp.value;
+  obj = {
+    "user": {
+      "email": email,
+      "nickname": nickname,
+      "password": password
+    }
+  };
+
+  axios.post(`${urlAPI}/users`, obj)
+    .then(function (response) {
+      Swal.fire({
+        title: '註冊成功',
+        icon: 'success',
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'ok'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          console.log('轉換')
+        }
+      });
+    })
+    .catch(function (err) {
+      Swal.fire({
+        icon: 'error',
+        title: `${err.response.data.message}`,
+      });
+    });
+};
+document.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.className === 'signup'){
+    if (accountSignUp.value === '' || nicknameSignUp.value === '' || passwordSignUp.value === ''){
+      return Swal.fire('請輸入值')
+    }
+    callSignUp();
+    accountSignUp.value = '';
+    nicknameSignUp.value = '';
+    passwordSignUp.value = '';
+  }
+});
