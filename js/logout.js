b09891c2da032b530a34651ff8c6c371d8706aad
Linlaose <a0986575797@gmail.com>
2022-07-04 21:07:37 +0800

    抽出渲染函式

--- js/logout.js
+++ js/logout.js
@@ -1,8 +1,9 @@
 const userName = document.querySelector('.todoInsert');
 
+function renderPage() {
+  userName.textContent = `${JSON.parse(localStorage.getItem('userName'))} 的待辦`;
+};
+
 if (document.body.className === 'todoPage') {
-  function renderPage() {
-    userName.textContent = `${JSON.parse(localStorage.getItem('userName'))} 的待辦`;
-  };
   renderPage();
 }
