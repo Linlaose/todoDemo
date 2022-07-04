860ba35699edb4600995512a25a9420ada794188
Linlaose <a0986575797@gmail.com>
2022-07-04 21:35:01 +0800

    增加登出功能

--- js/logout.js
+++ js/logout.js
@@ -1,8 +1,51 @@
 const userName = document.querySelector('.todoInsert');
+const accountLogOut = document.querySelector('.account-logOut');
+const passwordLogOut = document.querySelector('.password-logOut');
+const urlAPI = 'https://todoo.5xcamp.us';
+const token = JSON.parse(localStorage.getItem('token'));
 
 function renderPage() {
   userName.textContent = `${JSON.parse(localStorage.getItem('userName'))} 的待辦`;
 };
+
+function callLogOut() {
+  let obj = {
+    headers: {
+      Authorization: token
+    }
+  }
+  axios.delete(`${urlAPI}/users/sign_out`, obj)
+    .then (function (response) {
+      let timerInterval
+      Swal.fire({
+        title: 'Auto close alert!',
+        html: 'I will close in <b></b> milliseconds.',
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading()
+          const b = Swal.getHtmlContainer().querySelector('b')
+          timerInterval = setInterval(() => {
+            b.textContent = Swal.getTimerLeft()
+          }, 100)
+        },
+        willClose: () => {
+          clearInterval(timerInterval)
+        }
+      }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+          console.log('I was closed by the timer')
+        }
+      })
+    })
+    .catch(function (err) {
+      Swal.fire({
+        icon: 'error',
+        title: `${err.response.data.message}`,
+      });
+    });
+}
 
 if (document.body.className === 'todoPage') {
   renderPage();
