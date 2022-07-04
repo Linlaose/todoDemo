42d7da1660795dbe37d07649dff3fa0fc85d5e67
Linlaose <a0986575797@gmail.com>
2022-07-04 23:57:29 +0800

    渲染待辦事項清單

--- js/render.js
+++ js/render.js
@@ -1,12 +1,22 @@
 const userName = document.querySelector('.todoInsert');
 const token = JSON.parse(localStorage.getItem('token'));
+const todoList = document.querySelector('ul');
 const urlAPI = 'https://todoo.5xcamp.us';
 
 
 // 渲染畫面函式
 function renderPage() {
+  let el = '';
+  let list = JSON.parse(localStorage.getItem('list'));
+
   userName.textContent = `${JSON.parse(localStorage.getItem('userName'))} 的待辦`;
   getTodo();
+
+  list.forEach(function (item, index) {
+    el += `<li data-id=${item.id}>${item.content} <input type='button' data-num=${index} value='delete'></li>`;
+  });
+
+  todoList.innerHTML = el;
   console.log('render');
 };
 
